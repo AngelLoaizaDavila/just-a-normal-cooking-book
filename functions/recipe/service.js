dffcaf69e733fc3fbd83f53566d45a3d81edf3d5
Angel Loaiza Davila <loaizadavila.angeldario@gmail.com>
2024-02-08 22:34:23 +0100

    added update ingredient function

--- functions/recipe/service.js
+++ functions/recipe/service.js
@@ -58,7 +58,7 @@ module.exports.updateRecipe = async (data) => {
   }
 };
 module.exports.updateIngredients = async (data) => {
-  const {recipeName, ingredients} = data
+  const { recipeName, ingredients } = data;
   /**
    * ingredients: [
    *  {
@@ -70,49 +70,68 @@ module.exports.updateIngredients = async (data) => {
    * ]
    */
   if (!recipeName) {
-    throw createError(400, "Missing required params")
+    throw createError(400, "Missing required params");
   }
   if (!Array.isArray(ingredients)) {
-    throw createError(400, "Missing required params")
+    throw createError(400, "Missing required params");
   }
-  await db.init()
-  const recipe = await Recipe.findOne({name: recipeName})
+  await db.init();
+  const recipe = await Recipe.findOne({ name: recipeName });
   if (!recipe) {
-    throw createError(404, "Recipe does not exist")
+    throw createError(404, "Recipe does not exist");
   }
-
+  const updatedIngredients = [];
+  for (let i = 0; i < recipe.ingredients.length; i++) {
+    // oldIngredient
+    const ingredient = recipe.ingredient[i];
+    // newIngredient -> if exists and has been modified
+    const existIngredient = ingredients.find(
+      (item) => item.oldName === ingredient.name
+    );
+    // If ingredient does not exist in modified ingredients array, maintain it
+    if (!existIngredient) {
+      updatedIngredients.push(ingredient);
+      continue;
+    }
+    // If ingredient exist in modified ingredients array, modify and save it
+    if (existIngredient) {
+      updatedIngredients.push(
+        checkIngredientModificationAndReturnModifiedIngredient(
+          ingredient,
+          existIngredient
+        )
+      );
+    }
+  }
+  await Recipe.updateOne(
+    {
+      name: recipeName,
+    },
+    {
+      $set: {
+        ingredients: updatedIngredients,
+      },
+    }
+  );
+  return updatedIngredients;
 };
 
-const updateIngredients = (oldIngredients, newIngredients) => {
-  const updatedIngredients = oldIngredients;
-
-  for (let i = 0; i < updatedIngredients.length; i++) {
-    const oldIngredient = updatedIngredients[i];
-    const alreadyExistsNewIng = newIngredients.find(
-      (item) =>
-        item.name === oldIngredient.name ||
-        oldIngredient.name.includes(item.name)
-    );
-    if (alreadyExistsNewIng) {
-      updatedIngredients[i] = {
-        name:
-          alreadyExistsNewIng.name !== oldIngredient.name
-            ? newIngredient.name
-            : alreadyExists.name,
-        quantity:
-          newIngredient.quantity !== alreadyExists.quantity
-            ? newIngredient.quantity
-            : alreadyExists.quantity,
-        unit:
-          newIngredient.unit !== alreadyExists.unit
-            ? newIngredient.unit
-            : alreadyExists.unit,
-      };
-    }
-    if (!alreadyExists) {
-      updatedIngredients.push(newIngredient);
-    }
-  }
-
-  return updatedIngredients;
+const checkIngredientModificationAndReturnModifiedIngredient = (
+  oldIngredient,
+  newIngredient
+) => {
+  return {
+    name:
+      newIngredient.newName !== null && newIngredient.newName !== undefined
+        ? newIngredient.newName
+        : oldIngredient.oldName,
+    quantity:
+      newIngredient.quantity !== null && newIngredient.quantity !== undefined
+        ? newIngredient.quantity
+        : oldIngredient.quantity,
+    unit:
+      newIngredient.unit !== null && newIngredient.unit !== undefined
+        ? newIngredient.unit
+        : oldIngredient.unit,
+  };
 };
